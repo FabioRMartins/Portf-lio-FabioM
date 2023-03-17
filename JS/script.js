b227181c582483fcb3f9681c99786191aa9a63e6
FabioMartins <fabbio1000@gmail.com>
2023-03-17 10:06:00 -0300

    Feat: Adicionando função de copiar email

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,18 @@
 document.querySelector(".button_header").addEventListener("click", ()=> document.querySelector(".menu_header").classList.toggle("active"));
+
+document.querySelectorAll(".copy_link").forEach((copyLinkParent) => {
+    const inputField = copyLinkParent.querySelector(".copy_link_input");
+    const copyButton = copyLinkParent.querySelector(".copy_link_button");
+    const text = inputField.value;
+  
+    inputField.addEventListener("focus", () => inputField.select());
+  
+    copyButton.addEventListener("click", () => {
+      inputField.select();
+      navigator.clipboard.writeText(text);
+  
+      inputField.value = "Copied!";
+      setTimeout(() => (inputField.value = text), 800);
+    });
+  });
+  
